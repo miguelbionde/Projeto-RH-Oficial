d3c7c54e47541267986a5811b2214d3bf37c0093
miguelbionde <miiguel.b.paulo@gmail.com>
2023-05-19 21:51:33 -0300

    add desconto inss

--- iRh/iRh.Windows/Simuladores/frmDescontoInss.Designer.cs
+++ iRh/iRh.Windows/Simuladores/frmDescontoInss.Designer.cs
@@ -28,19 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.panelResultado = new System.Windows.Forms.Panel();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.panelResultado.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(34, 28);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(124, 30);
+            this.lblSalario.TabIndex = 0;
+            this.lblSalario.Text = "Salário Base";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(39, 73);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(213, 35);
+            this.txtSalario.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(39, 151);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(118, 62);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular INSS";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // panelResultado
+            // 
+            this.panelResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panelResultado.Controls.Add(this.lblResultado);
+            this.panelResultado.Location = new System.Drawing.Point(39, 249);
+            this.panelResultado.Name = "panelResultado";
+            this.panelResultado.Size = new System.Drawing.Size(318, 128);
+            this.panelResultado.TabIndex = 3;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(22, 21);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(126, 30);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "lblResultado";
             // 
             // frmDescontoInss
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(417, 407);
+            this.Controls.Add(this.panelResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.lblSalario);
             this.Name = "frmDescontoInss";
-            this.Text = "Desconto INSS";
+            this.Text = "Desconto de INSS";
+            this.panelResultado.ResumeLayout(false);
+            this.panelResultado.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Panel panelResultado;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
--- iRh/iRh.Windows/Simuladores/frmDescontoInss.cs
+++ iRh/iRh.Windows/Simuladores/frmDescontoInss.cs
@@ -16,5 +16,30 @@ namespace iRh.Windows.Simuladores
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSalario.Text)) 
+            {
+                MessageBox.Show("Informe seu Salário base!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtSalario.Focus();
+                return;
+            }
+
+
+            try
+            {
+                var salario = double.Parse(txtSalario.Text);
+                if (salario <= 1302.00)
+                    var desconto_1
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("informe um valor de salário Válido, ex 3500", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            
+
+        }
     }
 }
